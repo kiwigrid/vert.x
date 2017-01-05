9926f02c155309eaa616dd58f4c598e1f99ef72e
Mirko Seifert <mirko.seifert@kiwigrid.com>
2017-01-05 21:37:18 +0100

    - Formatted code

--- vertx-core/src/main/java/org/vertx/java/core/http/impl/UriParser.java
+++ vertx-core/src/main/java/org/vertx/java/core/http/impl/UriParser.java
@@ -23,48 +23,48 @@ package org.vertx.java.core.http.impl;
  */
 final class UriParser {
 
-  private UriParser() {
+	private UriParser() {
 
-  }
+	}
 
-  /**
-   * Extract the path out of the uri.
-   *
-   */
-  static String path(String uri) {
-	  int i;
-	  if (uri.charAt(0) == '/') {
-		  i = 0;
-	  } else {
-		  i = uri.indexOf("://");
-		  if (i == -1) {
-			  i = 0;
-		  } else {
-			  i = uri.indexOf('/', i + 3);
-			  if (i == -1) {
-				  // contains no /
-				  return "/";
-			  }
-		  }
-	  }
+	/**
+	 * Extract the path out of the uri.
+	 *
+	 */
+	static String path(String uri) {
+		int i;
+		if (uri.charAt(0) == '/') {
+			i = 0;
+		} else {
+			i = uri.indexOf("://");
+			if (i == -1) {
+				i = 0;
+			} else {
+				i = uri.indexOf('/', i + 3);
+				if (i == -1) {
+					// contains no /
+					return "/";
+				}
+			}
+		}
 
-	  int queryStart = uri.indexOf('?', i);
-	  if (queryStart == -1) {
-		  queryStart = uri.length();
-	  }
-	  return uri.substring(i, queryStart);
-  }
+		int queryStart = uri.indexOf('?', i);
+		if (queryStart == -1) {
+			queryStart = uri.length();
+		}
+		return uri.substring(i, queryStart);
+	}
 
-  /**
-   * Extract the query out of a uri or returns {@code null} if no query was found.
-   */
-  static String query(String uri) {
-    int i = uri.indexOf('?');
-    if (i == -1) {
-      return null;
-    } else {
-      return uri.substring(i + 1 , uri.length());
-    }
-  }
+	/**
+	 * Extract the query out of a uri or returns {@code null} if no query was found.
+	 */
+	static String query(String uri) {
+		int i = uri.indexOf('?');
+		if (i == -1) {
+			return null;
+		} else {
+			return uri.substring(i + 1 , uri.length());
+		}
+	}
 
 }
